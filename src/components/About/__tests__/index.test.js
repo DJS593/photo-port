a74e6da7559d7ae010db12d8d45b9f543d0f67ba
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-07-20 10:42:55 -0700

    test complete on About

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
+import { hasUncaughtExceptionCaptureCallback } from 'process';
 
 
 
@@ -16,6 +17,9 @@ describe('About component', () => {
 
   it('matches snapshot DOM node structure', () => {
     // render About
+    const { asFragment } = render(<About />);
+
+    expect(asFragment()).toMatchSnapshot();
   })
 })
 
